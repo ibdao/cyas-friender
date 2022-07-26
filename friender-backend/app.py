1753eecb57e3f69a1adc01f8f12f540c35e6bf05
Ian Dao <ibdao921@gmail.com>
2022-07-26 16:26:26 -0700

    working on uploading files

--- friender-backend/app.py
+++ friender-backend/app.py
@@ -1,33 +1,45 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-import boto3
-from botocore.exceptions import NoCredentialsError
+import boto3, botocore
+import os
+from botocore.exceptions import ClientError
+
+from forms import UploadForm
 
 app = Flask(__name__)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+BUCKET = "friender-bucket-r26-jason"
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=app.config['S3_KEY'],
+   aws_secret_access_key=app.config['S3_SECRET']
+)
 
 #debug = DebugToolbarExtension(app)
 RESPONSES_KEY = "responses"
+
 
 @app.get("/")
 def generate_landing():
     """Generate app landing page"""
 
-    return render_template (
+    return render_template(
         "base.html",
-        )
+    )
+
 
 @app.post("/")
-def handle_get_request_and_render_form():
-    """Upload to aws"""
-    def upload(local_file, bucket, s3_file):
-        s3 = boto3.client(
-            "s3",
-            aws_access_key_id = ACCESS_KEY,
-            aws_secret_access_key = SECRET_KEY
-        )
-
-
-    return redirect (
-        "/"
-    )
+def upload_file():
+    if "user_file" not in request.files:
+        return "No user_file key in request.files"
+    file = request.files["user_file"]
+    if file.filename == "":
+        return "Please select a file"
+    if file:
+        file.filename = secure_filename(file.filename)
+        output = send_to_s3(file, app.config["S3_BUCKET"])
+        return str(output)
+    else:
+        return redirect("/")
